82997e72c5a1205dea4e11053abd1c59d1cce758
Christian Budde Christensen <budde377@gmail.com>
2018-03-02 15:51:19 +0100

    feat: Colors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // @flow
 
 import Koa from 'koa'
+import serve from 'koa-static'
+import path from 'path'
 
 const app = new Koa()
 
-app.use((ctx: *) => {
-  ctx.body = 'Hello world :)'
-})
+app.use(serve(path.join(__dirname, '..', 'static')))
 
 export default app
