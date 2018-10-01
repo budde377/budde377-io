4da0afc523c4c425f353549b16cd7db4dd8d3256
Christian Budde Christensen <budde377@gmail.com>
2018-10-01 14:35:19 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,7 +29,8 @@ npm run build
 docker build -t budde377/budde377-io:$TRAVIS_COMMIT .
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker push budde377/budde377-io:$TRAVIS_COMMIT
-
+helm init -c
+helm init --upgrade
 helm upgrade --wait --timeout 300 -i \
   --set image.tag=$TRAVIS_COMMIT \
   --set server.key=$SERVER_KEY \
